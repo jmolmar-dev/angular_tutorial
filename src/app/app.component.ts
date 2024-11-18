a0e0e242c286280916bdf5245c2f3bfce939fea6
jmolmar-dev <jmolmar984@iescarrillo.es>
2024-11-18 18:19:50 +0100

    Desarrollo Rutas y Despliegue del Proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { FooterComponent } from './components/footer/footer.component';
 import { TasklistComponent } from "./components/task/tasklist/tasklist.component";
@@ -7,7 +7,7 @@ import { TasklistComponent } from "./components/task/tasklist/tasklist.component
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent, FooterComponent, TasklistComponent],
+  imports: [RouterOutlet,NavbarComponent, FooterComponent, TasklistComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
