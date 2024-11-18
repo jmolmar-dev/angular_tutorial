70a6f723175f9e97f92b13d6426f97926bd6376b
jmolmar-dev <jmolmar984@iescarrillo.es>
2024-11-18 18:04:19 +0100

    Desarrollo Paths

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { TasklistComponent } from "./components/task/tasklist/tasklist.component
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent, TasklistComponent],
+  imports: [RouterOutlet,NavbarComponent, FooterComponent, TasklistComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ListComponent } from './components/list/list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'list', component:ListComponent},
+    {path:'profile',component:ProfileComponent},
+    {path:'',redirectTo:'/list',pathMatch:'full'}
+];
